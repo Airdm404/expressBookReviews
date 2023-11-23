b6fc7f11b1d0ab60e9f0a339f6eef65e1e24ff69
Airdm404 <edemcapp101@gmail.com>
2023-11-23 04:38:56 -0500

    finish all major tasks

--- final_project/index.js
+++ final_project/index.js
@@ -6,25 +6,27 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next) {
     if (req.session.authorization) {
         token = req.session.authorization['accessToken'];
-        jwt.verify(token, 'access', (err,user) => {
+        jwt.verify(token, "access",(err,user) => {
             if(!err) {
                 req.user = user;
                 next()
             } else {
-                return res.status(404).json({message:"User not authenticated"})
+                return res.status(404).json({message:"customer not authenticated"})
             }
         });
     } else {
-        return res.status(403).json({message: "User not logged in"})
+        return res.status(403).json({message: "customer not logged in"})
     }
 });
+
  
 const PORT =5000;
 
